cd1efd1694aa206077b29083226cb2da778fdb08
Taoists8064 <1394091210@qq.com>
2019-11-12 21:17:07 +0800

    二零一九一月十二 维度项目

---
+++ app/src/main/java/com/bw/weidu/activitybase/BaseActivity.java
@@ -0,0 +1,22 @@
+package com.bw.weidu.activitybase;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(initLayout());
+        initView();
+        initData(savedInstanceState);
+    }
+
+    protected abstract void initData(Bundle savedInstanceState);
+
+
+    protected abstract void initView();
+
+    protected abstract int initLayout();
+}
