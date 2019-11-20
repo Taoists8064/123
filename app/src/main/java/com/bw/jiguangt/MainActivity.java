462f1de1089fe5d64aa73f6cdef44da7bcd22a48
Taoists8064 <1394091210@qq.com>
2019-11-20 09:03:21 +0800

    实现推送  看好了

---
+++ app/src/main/java/com/bw/jiguangt/MainActivity.java
@@ -0,0 +1,34 @@
+package com.bw.jiguangt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.bw.lianxiaaa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+        //开始推送
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show(); Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show(); Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show(); Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show(); Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+
+
+
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "我要开始推送", Toast.LENGTH_SHORT).show();
+
+
+
+
+
+
+
+    }
+}
