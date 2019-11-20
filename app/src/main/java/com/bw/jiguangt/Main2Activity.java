e42606315bcff2deb6e8dafaa1d9b5079cfa1947
Taoists8064 <1394091210@qq.com>
2019-11-20 09:07:39 +0800

    我要发送消息了  你看好了

---
+++ app/src/main/java/com/bw/jiguangt/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.bw.jiguangt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bw.lianxiaaa.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+        //我要发送推送消息了  看好了   我已成功接受到消息
+    }
+}
